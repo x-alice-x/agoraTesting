5e62a4bd50ecb64d05d8d21905ba20f3898171bc
Alisa <kozhemyakina_alice@smartworld.team>
2020-04-28 09:56:35 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<HelloWorld />
+		<canvas id="local_stream"></canvas>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import AgoraRTC from "agora-rtc-sdk";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			rtc: {
+				client: null,
+				joined: false,
+				published: false,
+				localStream: null,
+				remoteStreams: [],
+				params: {}
+			},
+			option: {
+				appID: process.env.VUE_APP_ID,
+				// appID: "c414bc5c83954aec8467addd2cc43159",
+				channel: process.env.VUE_APP_CHANNEL,
+				// channel: "qwe",
+				uid: null,
+				token: process.env.VUE_APP_TOKEN
+				// token: "c414bc5c83954aec8467addd2cc43159"
+			}
+		};
+	},
+	mounted() {
+		let self = this;
+		// Create a client
+		this.rtc.client = AgoraRTC.createClient({ mode: "rtc", codec: "h264" });
+		console.log(this.option);
+
+		// Initialize the client
+		this.rtc.client.init(
+			this.option.appID,
+			function() {
+				console.log("init success");
+
+				// Join a channel
+				self.rtc.client.join(
+					self.option.token,
+					self.option.channel,
+					self.option.uid,
+					function(uid) {
+						console.log(
+							"join channel: " +
+								self.option.channel +
+								" success, uid: " +
+								uid
+						);
+						self.rtc.params.uid = uid;
+
+						// Create a local stream
+						self.rtc.localStream = AgoraRTC.createStream({
+							streamID: self.rtc.params.uid,
+							audio: true,
+							video: true,
+							screen: false
+						});
+						console.log(self.rtc.localStream);
+
+						// Initialize the local stream
+						self.rtc.localStream.init(
+							function() {
+								console.log("init local stream success");
+								// play stream with html element id "local_stream"
+								self.rtc.localStream.play("local_stream");
+
+								// Publish the local stream
+								self.rtc.client.publish(
+									self.rtc.localStream,
+									function(err) {
+										console.log("publish failed");
+										console.error(err);
+									}
+								);
+							},
+							function(err) {
+								console.error("init local stream failed ", err); // device's not found (maybe bc of camera and microphone)
+							}
+						);
+					},
+					function(err) {
+						console.error("client join failed", err);
+					}
+				);
+			},
+			err => {
+				console.error(err);
+			}
+		);
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+canvas {
+	width: 500px;
+	height: 500px;
 }
 </style>
